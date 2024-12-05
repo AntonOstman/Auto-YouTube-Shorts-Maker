436a0181d3f272e1f6fcebc1106aa6e65c9e4b7f
Anton <anton.ostman123@gmail.com>
2024-12-05 07:44:32 +0100

    Add subtitles to generator

--- shorts.py
+++ shorts.py
@@ -6,13 +6,16 @@ import openai
 from gtts import gTTS
 from moviepy.editor import *
 import moviepy.video.fx.crop as crop_vid
+from moviepy.video.tools.subtitles import SubtitlesClip
 load_dotenv()
 
 # Ask for video info
-title = input("\nEnter the name of the video >  ")
-option = input('Do you want AI to generate content? (yes/no) >  ')
+# title = input("\nEnter the name of the video >  ")
+title = "test"
+# option = input('Do you want AI to generate content? (y/n) >  ')
+option = ''
 
-if option == 'yes':
+if option == 'y':
     # Generate content using OpenAI API
     theme = input("\nEnter the theme of the video >  ")
 
@@ -29,40 +32,65 @@ if option == 'yes':
     )
     print(response.choices[0].text)
 
-    yes_no = input('\nIs this fine? (yes/no) >  ')
-    if yes_no == 'yes':
+    yes_no = input('\nIs this fine? (y/n) >  ')
+    if yes_no == 'y':
         content = response.choices[0].text
     else:
         content = input('\nEnter >  ')
 else:
-    content = input('\nEnter the content of the video >  ')
+    # Read sample file and create the sentence from each line
+    file = open("sample-text/short-sample.txt")
+    sentences = file.read().split('\n')
+    content = ' '.join(sentence for sentence in sentences)
 
 # Create the directory
 if os.path.exists('generated') == False:
     os.mkdir('generated')
 
+max_video_length = 58
+video_duration = max_video_length
+
 # Generate speech for the video
-speech = gTTS(text=content, lang='en', tld='ca', slow=False)
-speech.save("generated/speech.mp3")
+def create_voice(content):
+    generate_voice = False
+    # if (generate_voice):
+    speech = gTTS(text=content, lang='en', tld='ca', slow=False)
+    speech.save("generated/speech.mp3")
 
-gp = random.choice(["1", "2"])
-start_point = random.randint(1, 480)
-audio_clip = AudioFileClip("generated/speech.mp3")
+    audio_clip = AudioFileClip("generated/speech.mp3")
+    audio_clip = audio_clip.fx( vfx.speedx, 1) 
 
-if (audio_clip.duration + 1.3 > 58):
-    print('\nSpeech too long!\n' + str(audio_clip.duration) + ' seconds\n' + str(audio_clip.duration + 1.3) + ' total')
-    exit()
+    new_content = content
+    while (audio_clip.duration > video_duration):
+        print(f'\nSpeech too long!\n {str(audio_clip.duration)} seconds \n {str(audio_clip.duration)} total')
+        scale_diff = video_duration / audio_clip.duration
+        remove_amount = int(len(new_content) * scale_diff) - 10
+        new_content = new_content[:remove_amount]
 
-print('\n')
+        speech = gTTS(text=new_content, lang='en', tld='ca', slow=False)
+        speech.save("generated/speech.mp3")
 
+        audio_clip = AudioFileClip("generated/speech.mp3")
+        audio_clip = audio_clip.fx( vfx.speedx, 1) 
+
+    return new_content, audio_clip
+
+print("Trying to find voiceover with correct length")
+audio_ok = False
+
+
+print("Editing video")
 ### VIDEO EDITING ###
-
+content, audio_clip = create_voice(content)
 # Trim a random part of minecraft gameplay and slap audio on it
-video_clip = VideoFileClip("gameplay/gameplay_" + gp + ".mp4").subclip(start_point, start_point + audio_clip.duration + 1.3)
-final_clip = video_clip.set_audio(audio_clip)
+video = VideoFileClip(f"gameplay/minecraft_parkour.mkv")
+start_point = random.randint(1, int(video.duration) - video_duration)
+
+video_clip = video.subclip(start_point, start_point + audio_clip.duration)
+video_clip = video_clip.set_audio(audio_clip)
 
 # Resize the video to 9:16 ratio
-w, h = final_clip.size
+w, h = video_clip.size
 target_ratio = 1080 / 1920
 current_ratio = w / h
 
@@ -71,13 +99,40 @@ if current_ratio > target_ratio:
     new_width = int(h * target_ratio)
     x_center = w / 2
     y_center = h / 2
-    final_clip = crop_vid.crop(final_clip, width=new_width, height=h, x_center=x_center, y_center=y_center)
+    final_clip = crop_vid.crop(video_clip, width=new_width, height=h, x_center=x_center, y_center=y_center)
 else:
     # The video is taller than the desired aspect ratio, crop the height
     new_height = int(w / target_ratio)
     x_center = w / 2
     y_center = h / 2
-    final_clip = crop_vid.crop(final_clip, width=w, height=new_height, x_center=x_center, y_center=y_center)
+    final_clip = crop_vid.crop(video_clip, width=w, height=new_height, x_center=x_center, y_center=y_center)
+
+print("Adding subtitles to video")
+
+generator = lambda txt: TextClip(txt, align='center', size=(1000,None), method='caption', font='Montserrat', fontsize=70, color='white')
+# generator2 = lambda txt: TextClip(txt, font='Arial', fontsize=110, color='black').
+
+shown_words = 8
+wordized = content.split()
+# text_speedup = video_clip.duration * 0.10 # To display the subs a bit before the voice
+wps = len(content.split()) / ( video_clip.duration)
+
+divided_words = [wordized[i:i+shown_words] for i in range(0, len(wordized), shown_words)]
+
+subs = []
+for i, words in enumerate(divided_words):
+    offset = 0.8
+    subs_per_second = shown_words/wps
+    sub_start = max(0, i*subs_per_second - offset)
+    sub_end = min(i*subs_per_second + subs_per_second - offset, video_clip.duration)
+    subs.append(((sub_start, sub_end), ' '.join(map(str, words))))
+    print(subs[i][0])
+
+    
+subtitles = SubtitlesClip(subs, generator)
+# subtitles2 = SubtitlesClip(subs, generator2)
+# video_clip = CompositeVideoClip([video_clip, subtitles2.set_pos(('center'))])
+video_clip = CompositeVideoClip([video_clip, subtitles.set_pos(('center'))])
 
 # Write the final video
-final_clip.write_videofile("generated/" + title + ".mp4", codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
+video_clip.write_videofile("generated/" + title + ".mp4", codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
